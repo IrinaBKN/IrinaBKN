3224e4d0e407832d1486609fa62ec56a24a410d4
IrinaBKN <77120410+IrinaBKN@users.noreply.github.com>
2021-10-14 17:14:49 +0300

    Update checkTemp.js

--- HOMEWORK/checkTemp.js
+++ HOMEWORK/checkTemp.js
@@ -17,17 +17,14 @@ function checkTemp(x){
 /// teine ülesanne
 function checkTemp(x){
 			if (x>=0 && x<=20){
-				console.log("On liiga külm")
 				return (-1)
 			}
-			if (x>=21 && x<=41)
+			if (x>=21 && x<=40)
 			{
-				console.log("on paras temperatuur")
 				return (0)
 			} 
 			if (x>=41 && x<=60)
 			{
-				console.log("on liiga soe ")
 				return (1)
 			}
 		}
